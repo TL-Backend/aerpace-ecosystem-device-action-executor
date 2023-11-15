cce0dc9da99f119924413064215937d6bd15ce7e
Satyanarayana Raju <133110337+satyanarayanaRaju21@users.noreply.github.com>
2023-11-15 12:45:55 +0530

    initial code for device executor

--- src/routes/executor.route.js
+++ src/routes/executor.route.js
@@ -1,5 +1,6 @@
 const { execute } = require("../executor/executor");
+const { executorInputValidator } = require("../executor/executor.middleware");
 
 module.exports = function (app) {
-  app.get("/execute", execute);
+  app.post("/devices/execute", executorInputValidator, execute);
 };
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,28 @@
+exports.dbTables = {
+  ROLES_TABLE: "aergov_roles",
+  USER_ROLES_TABLE: "aergov_user_roles",
+  USERS_TABLE: `aergov_users`,
+  DEVICE_USERS: `aergov_device_users`,
+  DEVICE_TABLE: `aergov_devices`,
+  DEVICE_MODEL_PRIVILEGES: `aergov_device_model_privileges`,
+};
+
 exports.successResponses = {
   HEALTH_CHECK_SUCCESS: `Health check passed`,
+  DEVICE_COMMUNICATED_SUCCESSFULLY: `Device communicated successfully`,
 };
 
 exports.errorResponses = {
   SOMETHING_WENT_WRONG: `Something went wrong`,
+  ACTION_TYPE_NOT_FOUND: `Action type not found`,
+  NO_ACCESS: `Unauthorized to perform the operation`,
+  INVALID_PARAMETER: `Parameter not valid for given action`,
+  PARAMETERS_REQUIRED: `Parameters required for given action`,
+  DEVICE_NOT_FOUND: `Device not found`,
+  DEVICE_REQUIRED: `Device_id is missing or device_id invalid`,
+  ACTION_REQUIRED: `Action is missing or action invalid`,
+};
+
+exports.actionParameters = {
+  "AC#TURN_ON": { temperature: "number", mode: "string" },
 };
